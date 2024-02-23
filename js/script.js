1076ac0ead9948ee092ff5c5cdb471a34906901d
Yevhenii <zhenia1717@gmail.com>
2024-02-23 17:21:23 +0200

    add json file

--- js/script.js
+++ js/script.js
@@ -72,7 +72,7 @@ const spollers = document.querySelectorAll('[data-spoller]')
 
 if (spollers.length) {
     spollers.forEach(spoller => {
-        spoller.nextElementSibling.hidden = true
+        spoller.dataset.spoller !== "open" ? spoller.nextElementSibling.hidden = true : spoller.classList.add('active')
     })
 }
 
@@ -243,4 +243,91 @@ if (reviewsSlider) {
             }
         }
     });
+}
+
+// -----
+// Filter
+// -----
+
+// Price
+const filterRange = document.querySelector('.price-filter__range');
+if (filterRange) {
+    const filterRangeFrom = document.querySelector('.price-filter__input--from');
+    const filterRangeTo = document.querySelector('.price-filter__input--to');
+    noUiSlider.create(filterRange, {
+        start: [0, 100],
+        connect: true,
+        range: {
+            'min': 0,
+            'max': 100
+        },
+        format: wNumb({
+            decimals: 0,
+            thousand: '',
+            prefix: '$ '
+        })
+    });
+
+    filterRange.noUiSlider.on('update', function (values, handle) {
+       filterRangeFrom.value = `${values[0]}`
+       filterRangeTo.value = `${values[1]}`
+    })
+
+    filterRangeFrom.addEventListener('change', function () {
+        filterRange.noUiSlider.setHandle(0, filterRangeFrom.value);
+    });
+    filterRangeTo.addEventListener('change', function () {
+        filterRange.noUiSlider.setHandle(1, filterRangeTo.value);
+    });
+}
+
+// Catalog
+const catalogItems = document.querySelector('.catalog__items')
+if (catalogItems) {
+    loadProducts()
+}
+
+async function loadProducts() {
+    const response = await fetch("json/products.json", {
+        method: "GET"
+    })
+    if (response.ok) {
+        const responseData = await response.json()
+        initProducts(responseData)
+    } else {
+        alert('Error!')
+    }
+}
+
+function initProducts(data) {
+    const productsList = data.products
+    if (productsList.length) {
+        let productTemplate = ``;
+        productsList.forEach(productItem => {
+            productTemplate += `<article class="item-product">`
+            productTemplate += `<a href="#" class="item-product__favorite ${productItem.favorite ? 'item-product__favorite--active' : null} _icon-favorite"></a>`
+            if (productItem.image) {
+                productTemplate += `<a href="${productItem.url}" class="item-product__picture-link">`
+                productTemplate += `<img src="${productItem.image}" class="item-product__image"  alt="Image">`
+                productTemplate += `</a>`
+            }
+            productTemplate += `<div class="item-product__body">`
+            if (productItem.title) {
+                productTemplate += `<h4 class="item-product__title">`
+                productTemplate += `<a href="${productItem.url}" class="item-product__link-title">${productItem.title}</a>`
+                productTemplate += `</h4>`
+            }
+            if (productItem.label) {
+                productTemplate += `<div class="item-product__label">${productItem.label}</div>`
+            }
+            if (productItem.price) {
+                productTemplate += ` <div class="item-product__price">${productItem.price}}</div>`
+            }
+            productTemplate += `</div>`
+            productTemplate += `</article>`
+            
+        })
+        catalogItems.innerHTML = productTemplate
+        
+    }
 }
